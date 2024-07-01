528ea0ecc9bb38ddbf931c49c078f196b1d856d1
aumu1530 <aumu1530@gmail.com>
2024-07-01 20:59:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let role = 'umid' 
+ 
+ 
+let txt = prompt("Yoshingizni kiriting") 
+if (txt == 18) { 
+    document.body.style.backgroundColor = 'lime' 
+    document.title = 'bolalar uchun !' 
+} else { 
+    document.body.style.backgroundColor = 'crimson' 
+    document.title = 'kotalar uchun !' 
+}
